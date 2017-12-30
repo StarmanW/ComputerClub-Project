387e642fd99a4bde86e1943a808607d957ee8d32
StarmanW <swkf3011@hotmail.com>
2017-12-30 11:05:56 +0800

    MemberDA

--- ComputerClub/src/java/dataAccess/MemberDA.java
+++ ComputerClub/src/java/dataAccess/MemberDA.java
@@ -20,7 +20,6 @@ public class MemberDA {
     private final String dbUser = "nbuser";
     private final String dbPass = "nbuser";
     private final String tableName = "MEMBERS";
-    
     private ProgrammeDA programmeDA;
     
     //No-arg constructor
@@ -37,38 +36,111 @@ public class MemberDA {
         }
     }
     
+    //Method to select a specific record
+    public void selectRecord(String studID) {
+        try {
+            pstmt = conn.prepareStatement("SELECT * FROM " + tableName + " WHERE MEMBERID = ?");
+            pstmt.setString(1, studID);
+            rs = pstmt.executeQuery();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
     //Method to create new recored
-    public int createRecord(Member member) throws Exception {
-        pstmt = conn.prepareStatement("INSERT INTO " + tableName + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        pstmt.setString(1, member.getStudID());
-        pstmt.setString(2, member.getProgramme().getProgID());
-        pstmt.setString(3, member.getStudName().getFirstName());
-        pstmt.setString(4, member.getStudName().getLastName());
-        pstmt.setString(5, member.getEmail());
-        pstmt.setString(6, member.getContactNo());
-        pstmt.setString(7, member.getIc());
-        pstmt.setString(8, member.getPassword());
-        pstmt.setString(9, String.valueOf(member.getGender()));
-        pstmt.setBoolean(10, member.isMembershipFeeStatus());
-        pstmt.setInt(11, member.getPosition());
-        pstmt.setString(12, member.getAcademicYear());
-        int successInsert = pstmt.executeUpdate();
+    public int createRecord(Member member) {
+        int successInsert = 0;
+        
+        try {
+            selectRecord(member.getStudID());
+            if (rs.next()) {
+                successInsert = -1;
+            } else {
+                pstmt = conn.prepareStatement("INSERT INTO " + tableName + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                pstmt.setString(1, member.getStudID());
+                pstmt.setString(2, member.getProgramme().getProgID());
+                pstmt.setString(3, member.getStudName().getFirstName());
+                pstmt.setString(4, member.getStudName().getLastName());
+                pstmt.setString(5, member.getEmail());
+                pstmt.setString(6, member.getContactNo());
+                pstmt.setString(7, member.getIc());
+                pstmt.setString(8, member.getPassword());
+                pstmt.setString(9, String.valueOf(member.getGender()));
+                pstmt.setBoolean(10, member.isMembershipFeeStatus());
+                pstmt.setInt(11, member.getPosition());
+                pstmt.setString(12, member.getAcademicYear());
+                successInsert = pstmt.executeUpdate();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         
         return successInsert;
     }
     
-    //Method to retrieve all records
-    public ArrayList<Member> retrieveAllMembers() {
-        ArrayList<Member> members = new ArrayList<Member>();
-        
+    //Method to delete a record
+    public int deleteRecord(Member member) {
+        int successDelete = 0;
         try {
-            pstmt = conn.prepareCall("SELECT * FROM MEMBERS");
+            selectRecord(member.getStudID());
+            if (rs.next()) {
+                pstmt = conn.prepareStatement("DELETE FROM " + tableName + " WHERE MEMBERID = ?");
+                pstmt.setString(1, member.getStudID());
+                successDelete = pstmt.executeUpdate();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return successDelete;
+    }
+    
+    
+    //Method to update a record
+    public int updateRecord(Member member) {
+        int successUpdate = 0;
+
+        try {
+            selectRecord(member.getStudID());
+            if (rs.next()) {
+                pstmt = conn.prepareStatement("UPDATE " + tableName + " SET MEMBERID = ?, PROGID = ?, FIRSTNAME = ?, LASTNAME = ?, EMAIL = ?, CONTACTNUM = ?, ICNUM = ?, PASS = ?, GENDER = ?, MEMFEESTATS = ?, POSITION = ?, ACADEMICYEAR = ? WHERE MEMBERID = ?");
+                pstmt.setString(1, member.getStudID());
+                pstmt.setString(2, member.getProgramme().getProgID());
+                pstmt.setString(3, member.getStudName().getFirstName());
+                pstmt.setString(4, member.getStudName().getLastName());
+                pstmt.setString(5, member.getEmail());
+                pstmt.setString(6, member.getContactNo());
+                pstmt.setString(7, member.getIc());
+                pstmt.setString(8, member.getPassword());
+                pstmt.setString(9, String.valueOf(member.getGender()));
+                pstmt.setBoolean(10, member.isMembershipFeeStatus());
+                pstmt.setInt(11, member.getPosition());
+                pstmt.setString(12, member.getAcademicYear());
+                pstmt.setString(13, member.getStudID());
+                successUpdate = pstmt.executeUpdate();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        return successUpdate;
+    }
+    
+    //Method to retrieve all records
+    public ArrayList<Member> selectAllMembers() {
+        ArrayList<Member> members = new ArrayList<Member>();
+        try {
+            programmeDA = new ProgrammeDA();
+            pstmt = conn.prepareCall("SELECT * FROM " + tableName);
             rs = pstmt.executeQuery();
-            while (rs.next()) {
-                members.add(new Member(
-                        rs.getString(1), programmeDA.retrieveProgramme(rs.getString(2)), new Name(rs.getString(3), rs.getString(4)), 
-                        rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9).charAt(0), rs.getBoolean(10), 
-                        rs.getInt(11), rs.getString(12)));
+            while (rs.next()) {         //ERROR HERE BECAUSE OF SELECTPROGRAMME
+                members.add(new Member(rs.getString(1), programmeDA.selectProgramme(rs.getString(2)), 
+                        new Name(rs.getString(3), rs.getString(4)), rs.getString(5), rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8), 
+                        rs.getString(9).charAt(0), 
+                        rs.getBoolean(10), 
+                        rs.getInt(11), 
+                        rs.getString(12)));
             }
         } catch (Exception ex) {
             ex.printStackTrace();
