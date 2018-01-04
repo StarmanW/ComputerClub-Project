ce00284d366d87494a9991287defff702f8dea68
StarmanW <swkf3011@hotmail.com>
2018-01-04 19:51:38 +0800

    Update to ProcessRegistrationEvent1.java
    Removed "ProcessRegistrationEvent.java" and merged to ProcessRegistrationEvent1.java

--- ComputerClub/src/java/controller/ProcessRegistrationEvent.java
+++ ComputerClub/src/java/controller/ProcessRegistrationEvent.java
@@ -1,26 +1,14 @@
 package controller;
 
-import dataAccess.EventCollaboratorDA;
-import dataAccess.EventDA;
-import dataAccess.EventItemDA;
-import dataAccess.EventMemberDA;
-import dataAccess.ItemDA;
-import dataAccess.MemberDA;
-import dataAccess.CollaboratorDA;
-import java.io.IOException;
-import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import model.EventCollaborator;
-import model.EventMember;
-import model.EventItem;
-import model.Event;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.http.*;
+import dataAccess.*;
+import model.*;
 
 /**
- *
  * @author StarmanW
  */
 @WebServlet(name = "ProcessRegistrationEvent", urlPatterns = {"/ProcessRegistrationEvent"})
@@ -36,63 +24,20 @@ public class ProcessRegistrationEvent extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        //Get values from request
-        String eventName = request.getParameter("eventName");
-        String eventDate = request.getParameter("eventDate");
-        String eventStartTime = request.getParameter("eventStartTime");
-        String eventEndTime = request.getParameter("eventEndTime");
-        String eventLocation = request.getParameter("eventLocation");
-        int eventType = Integer.parseInt(request.getParameter("eventType"));
-        String[] itemIDList = (String[]) request.getSession().getAttribute("itemIDList");
-//        String[] memberIDList = (String[]) request.getSession().getAttribute("memberIDList");
-//        String[] collabIDList = (String[]) request.getSession().getAttribute("collabIDList");
-
-        //Success variable declaration
-        int successEventInsert = 0;
-        int successEventItemInsert = 0;
-        int successEventMemberInsert = 0;
-        int successEventCollaboratorInsert = 0;
-
         try {
             instantiateAllDA();
             String eventID = generateEventID();
+            String[] itemIDList = (String[]) request.getSession().getAttribute("itemIDList");
+            String[] collabIDList = (String[]) request.getSession().getAttribute("collabIDList");
+            String[] memberIDList = (String[]) request.getSession().getAttribute("memberIDList");
 
-            //Create the new Event object and insert the new event record
-            Event event = new Event(eventID, eventName, eventType, eventDate, eventStartTime, eventEndTime, eventLocation);
-            successEventInsert = eventDA.createRecord(event);
+            int successEventInsert = insertEvent(request, eventID);
+            int successEventItemInsert = insertEventItem(request, eventID);
+            //int successEventCollaboratorInsert = insertEventCollaborator(request, eventID);
+            //int successEventMemberInsert = insertEventMember(request, eventID);
 
-            ArrayList<EventItem> eventItemList = null;
-            String eventItemID = null;
-
-            //For loop to create new EventItem object and insert all records
-            for (int i = 0; i < itemIDList.length; i++) {
-                eventItemList = eventItemDA.selectAllEventItemList();
-                if (!eventItemList.isEmpty()) {
-                    int lastID = Integer.parseInt(eventItemList.get(eventItemList.size() - 1).getEventItemID().substring(2, 6));
-                    eventItemID = String.format("EI%04d", (lastID + 1));
-                } else {
-                    eventItemID = String.format("EI%04d", 1);
-                }
-                EventItem eventItem = new EventItem(eventItemID, eventDA.selectRecord(eventID), itemDA.selectRecord(itemIDList[i]));
-                successEventItemInsert = eventItemDA.createRecord(eventItem);
-            }
-
-//            //For loop to create new EventMember object and insert all records
-//            for (int i = 0; i < itemIDList.length; i++) {
-//                EventMember eventMember = new EventMember(eventID, memberDA.selectRecord(memberIDList[i]), eventDA.selectRecord(eventID));
-//                successEventMemberInsert = eventMemberDA.createRecord(eventMember);
-//            }
-//
-//            //For loop to create new EventCollaborator object and insert all records
-//            for (int i = 0; i < itemIDList.length; i++) {
-//                EventCollaborator eventCollaborator = new EventCollaborator(eventID, eventDA.selectRecord(eventID), collaboratorDA.selectRecord(collabIDList[i]));
-//                successEventCollaboratorInsert = eventCollaboratorDA.createRecord(eventCollaborator);
-//            }
             //Insert validation
-            //STUB
-            //if (all success insert value is correct) { redirect back to registerEvent.jsp?success }
-            if ((successEventInsert == 1) && (successEventItemInsert == itemIDList.length - 1)) {
+            if ((successEventInsert == 1) && (successEventItemInsert == itemIDList.length)) {
                 response.sendRedirect("registerEvent.jsp?success");
             }
         } catch (Exception ex) {
@@ -123,6 +68,117 @@ public class ProcessRegistrationEvent extends HttpServlet {
         } else {
             eventID = String.format("E%04d", 1);
         }
+
         return eventID;
+    }
+
+    //Method to insert event
+    private int insertEvent(HttpServletRequest request, String eventID) throws ServletException, IOException {
+        int successEventInsert = 0;
+
+        String eventName = request.getParameter("eventName");
+        String eventDate = request.getParameter("eventDate");
+        String eventStartTime = request.getParameter("eventStartTime");
+        String eventEndTime = request.getParameter("eventEndTime");
+        String eventLocation = request.getParameter("eventLocation");
+        int eventType = Integer.parseInt(request.getParameter("eventType"));
+
+        //Create the new Event object and insert the new event record
+        Event event = new Event(eventID, eventName, eventType, eventDate, eventStartTime, eventEndTime, eventLocation);
+        try {
+            eventDA.createRecord(event);
+            successEventInsert++;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return successEventInsert;
+    }
+
+    //Method to insert EventCollaborator
+    private int insertEventCollaborator(HttpServletRequest request, String eventID) throws ServletException, IOException {
+        int successEventCollaboratorInsert = 0;
+
+        String[] collabIDList = (String[]) request.getSession().getAttribute("collabIDList");
+        ArrayList<EventCollaborator> eventCollaboratorList = null;
+        String eventCollaboratorID = null;
+
+        //For loop to create new EventMember object and insert all records
+        for (int i = 0; i < collabIDList.length; i++) {
+            eventCollaboratorList = eventCollaboratorDA.selectAllEventCollabList();
+
+            if (!eventCollaboratorList.isEmpty()) {
+                int lastID = Integer.parseInt(eventCollaboratorList.get(eventCollaboratorList.size() - 1).getEventColladID().substring(2, 6));
+                eventCollaboratorID = String.format("EC%04d", (lastID + 1));
+            } else {
+                eventCollaboratorID = String.format("EC%04d", 1);
+            }
+
+            EventCollaborator eventCollaborator = new EventCollaborator(eventCollaboratorID, eventDA.selectRecord(eventID), collaboratorDA.selectRecord(collabIDList[i]));
+
+            try {
+                eventCollaboratorDA.createRecord(eventCollaborator);
+                successEventCollaboratorInsert++;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return successEventCollaboratorInsert;
+    }
+
+    //Method to insert EventItem
+    private int insertEventItem(HttpServletRequest request, String eventID) throws ServletException, IOException {
+        int successEventItemInsert = 0;
+
+        String[] itemIDList = (String[]) request.getSession().getAttribute("itemIDList");
+        ArrayList<EventItem> eventItemList = null;
+        String eventItemID = null;
+
+        //For loop to create new EventItem object and insert all records
+        for (int i = 0; i < itemIDList.length; i++) {
+            eventItemList = eventItemDA.selectAllEventItemList();
+
+            if (!eventItemList.isEmpty()) {
+                int lastID = Integer.parseInt(eventItemList.get(eventItemList.size() - 1).getEventItemID().substring(2, 6));
+                eventItemID = String.format("EI%04d", (lastID + 1));
+            } else {
+                eventItemID = String.format("EI%04d", 1);
+            }
+            EventItem eventItem = new EventItem(eventItemID, eventDA.selectRecord(eventID), itemDA.selectRecord(itemIDList[i]));
+            try {
+                eventItemDA.createRecord(eventItem);
+                successEventItemInsert++;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return successEventItemInsert;
+    }
+
+    //Method to insert EventMember
+    private int insertEventMember(HttpServletRequest request, String eventID) {
+        int successEventMemberInsert = 0;
+
+        String[] memberIDList = (String[]) request.getSession().getAttribute("memberIDList");
+        ArrayList<EventMember> eventMemberList = null;
+        String eventItemID = null;
+
+        //For loop to create new EventCollaborator object and insert all records
+        for (int i = 0; i < memberIDList.length; i++) {
+
+            if (!eventMemberList.isEmpty()) {
+                int lastID = Integer.parseInt(eventMemberList.get(eventMemberList.size() - 1).getEventMemberID().substring(2, 6));
+                eventItemID = String.format("EM%04d", (lastID + 1));
+            } else {
+                eventItemID = String.format("EM%04d", 1);
+            }
+            EventMember eventMember = new EventMember(eventItemID, memberDA.selectRecord(memberIDList[i]), eventDA.selectRecord(eventID));
+            try {
+                eventMemberDA.createRecord(eventMember);
+                successEventMemberInsert++;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return successEventMemberInsert;
     }
 }
