bbfa2f17fbb527f72a488e63d423b643ad5b7cd4
RemotePotato <RemotePotato@users.noreply.github.com>
2018-01-08 04:22:49 +0800

    Homepage now displays username

--- ComputerClub/src/java/controller/ProcessLogin.java
+++ ComputerClub/src/java/controller/ProcessLogin.java
@@ -8,12 +8,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import model.Login;
 import model.SHA256Hashing;
 
 /**
  *
  * @author Samuel Wong Kim Foong
+ * 
+ * Contributed by ChongJH
  */
 @WebServlet(name = "ProcessLogin", urlPatterns = {"/ProcessLogin"})
 public class ProcessLogin extends HttpServlet {
@@ -21,15 +24,19 @@ public class ProcessLogin extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LoginDA loginDA = new LoginDA();
-        SHA256Hashing sha256Hasing = new SHA256Hashing();
+        SHA256Hashing sha256Hashing = new SHA256Hashing();
         ArrayList<Login> loginList = loginDA.retrieveAllLogin();
-        
+
         boolean loginSuccess = false;
         String userName = request.getParameter("username");
         String password = request.getParameter("password");
 
+        HttpSession session = request.getSession();
+
         for (int i = 0; i < loginList.size(); i++) {
-            if (sha256Hasing.getHashedText(userName).equals(loginList.get(i).getUserName()) && sha256Hasing.getHashedText(password).equals(loginList.get(i).getUserPassword())) {
+            if (sha256Hashing.getHashedText(userName).equals(loginList.get(i).getUserName()) && sha256Hashing.getHashedText(password).equals(loginList.get(i).getUserPassword())) {
+                session.setAttribute("userName", userName);
+                
                 if (loginList.get(i).getUserRights().equals("admin")) {
                     response.sendRedirect("homepage.jsp");
                     loginSuccess = true;
@@ -41,7 +48,7 @@ public class ProcessLogin extends HttpServlet {
                 }
             }
         }
-        
+
         if (!loginSuccess) {
             response.sendRedirect("index.jsp?invalid");
         }
